4f5a912efa4c1990ad36e7083bd8f1111068d59a
Eiliv Hägg <eiliv@o7155a-06.o7155a.studat.rh66.ii.2.hb.s.cdal.chalmers.se>
2015-12-11 11:33:48 +0100

    new

--- k3_data/sourceK.m
+++
@@ -1,9 +0,0 @@
-function [Sp,Su] = sourceK(deltaY,dudy,vist,epsi,k,length)
-
-    Su = (vist .* (dudy).^2) .* deltaY;
-    
-    Sp = (-epsi./ k) .* deltaY;
-    
-    
-
-end
--- k3_data/sourceU.m
+++
@@ -1,6 +0,0 @@
-function [Sp,Su] = sourceU(deltaY,length)
-
-    Sp = zeros(length,1);
-    Su = ones(length,1) .* deltaY;
-
-end
